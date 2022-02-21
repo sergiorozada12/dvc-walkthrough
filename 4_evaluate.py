62a50282e2998f29e0804ff2f986345aa423f927
sergiorozada12 <sergiorozada@seedtag.com>
2022-02-21 12:18:36 +0100

    feature: added metrics to pipeline

---
+++ 4_evaluate.py
@@ -0,0 +1,36 @@
+import json
+from joblib import load
+import pandas as pd
+from sklearn import metrics
+import math
+import os
+
+os.makedirs(os.path.join("data", "metrics_threshold"), exist_ok=True)
+
+model = load("models/trained/model.joblib")
+
+df_test = pd.read_csv("data/prepared/wine_test.csv")
+X_test = df_test[df_test.columns[1:]]
+y_test = (df_test[df_test.columns[0]] == 3)*1
+
+y_pred = model.predict_proba(X_test)[:, 2]
+pre, rec, th = metrics.precision_recall_curve(y_test, y_pred)
+
+y_pred_labels = (model.predict(X_test) == 3)*1
+acc_avg = metrics.accuracy_score(y_test, y_pred_labels)
+pre_avg = metrics.precision_score(y_test, y_pred_labels, average='micro')
+rec_avg = metrics.recall_score(y_test, y_pred_labels, average='micro')
+
+# General metrics
+with open('data/metrics_threshold/scores.json', 'w') as f:
+    json.dump({"accuracy": acc_avg, "precision": pre_avg, "recall": rec_avg}, f)
+
+# Metrics by threshohld
+nth_point = math.ceil(len(th)/1000)
+test_results = list(zip(pre, rec, th))[::nth_point]
+with open("data/metrics_threshold/metrics.json", "w") as f:
+    json.dump(
+        {"prc": [{"precision": p, "recall": r, "threshold": t} for p, r, t in test_results]},
+        f,
+        indent=4,
+    )
--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 * ```dvc import https://github.com/sergiorozada12/dvc-walkthrough models/model.joblib```
 * ```dvc run -n prepare -p prepare.split -d 2_prepare.py -d data/wine.csv -o data/prepared python 2_prepare.py```
 * ```dvc run -n train -p train.seed -d 3_train_pipeline.py -d data/prepared/wine_train.csv -d data/prepared/wine_test.csv -o data/metrics -o models/trained python 3_train_pipeline.py```
+* ```dvc run -n evaluate -d 4_evaluate.py -d models/trained/model.joblib -d data/prepared/wine_test.csv -M data/metrics_threshold/scores.json --plots-no-cache data/metrics_threshold/metrics.json python 4_evaluate.py```
+* ```dvc repro```
+* ```dvc dag```
+* ```dvc metrics show```
+* ```dvc plots modify data/metrics_threshold/metrics.json -x recall -y precision```
+* ```dvc plots show```
